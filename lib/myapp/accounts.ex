3818b439d5f1d6d38a4b922998fe94177fd4e070
taka41s <gutonunes99@gmail.com>
2022-03-09 15:03:50 -0300

    validations, account registration in accounts controller

--- lib/myapp/accounts.ex
+++ lib/myapp/accounts.ex
@@ -13,4 +13,30 @@ defmodule Myapp.Accounts do
     |> User.registration_changeset(attrs)
     |> Repo.insert()
   end
+
+  def get_by_email(email) do
+    query = from u in User, where: u.email == ^email
+
+    case Repo.one(query) do
+      nil -> {:error, :not_found}
+      user -> {:ok, user}
+    end
+  end
+
+  def get_by_id!(id) do
+    User |> Repo.get!(id)
+  end
+
+  def authentication_user(email, password) do
+    with {:ok, user} <- get_by_email(email) do
+      case validate_password(password, user.password) do
+        false -> {:error, :unauthorized}
+        true -> {:ok, user}
+      end
+    end
+  end
+
+  defp validate_password(password, encrypted_password) do
+    Bcrypt.verify_pass(password, encrypted_password)
+  end
 end
